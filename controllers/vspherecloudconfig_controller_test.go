80838f568ddd039133bda17af96b92cdcf2c8717
asifdxtreme <smohammadasi@vmware.com>
2022-11-30 13:36:49 +0530

    Add VsphereCloudConfigReconciler Test Code

--- controllers/vspherecloudconfig_controller_test.go
+++ controllers/vspherecloudconfig_controller_test.go
@@ -19,6 +19,8 @@ package controllers
 import (
 	"context"
 	"crypto/tls"
+
+	vdov1alpha1 "github.com/vmware-tanzu/vsphere-kubernetes-drivers-operator/api/v1alpha1"
 	"time"
 
 	. "github.com/onsi/ginkgo"
@@ -138,10 +140,27 @@ var _ = Describe("VsphereCloudConfig controller", func() {
 				},
 			}
 
-			Expect(client.Create(ctx, secret)).Should(Succeed())
-
+			// When the secret is unknown
 			_, error := r.Reconcile(ctx, req)
+			Expect(error).To(HaveOccurred())
+			tempConfig := &vdov1alpha1.VsphereCloudConfig{}
+			r.Get(ctx, req.NamespacedName, tempConfig)
+			// Let the URL parsing Fail
+			tempConfig.Spec.VcIP = "DEL"
+			tempConfig.Spec.Thumbprint = ""
+			Expect(client.Create(ctx, secret)).Should(Succeed())
+			_, error = r.reconcileVCCredentials(ctx, tempConfig)
+			Expect(error).To(HaveOccurred())
+
+			// When all configs are available
+			//Expect(client.Create(ctx, secret)).Should(Succeed())
+			_, error = r.Reconcile(ctx, req)
 			Expect(error).ToNot(HaveOccurred())
+
+			// When the namespace is unknown
+			req.NamespacedName.Namespace = "nonexistent"
+			_, error = r.Reconcile(ctx, req)
+			Expect(error).To(HaveOccurred())
 
 			config := v1alpha1.VsphereCloudConfig{}
 			Eventually(func() v1alpha1.ConfigStatus {
